d359a3362b8eefb36029b760675ed5c36fcf498a
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2021-04-25 12:41:02 +0200

    Update paths resolution

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
-import path from "path";
+import { resolve } from "path";
 
 export default defineConfig({
-    alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "@js": path.resolve(__dirname, "./src/assets/js"),
-        "@css": path.resolve(__dirname, "./src/assets/css"),
-        "@images": path.resolve(__dirname, "./src/assets/images"),
-        "@icons": path.resolve(__dirname, "./src/assets/icons"),
-        "@components": path.resolve(__dirname, "./src/components"),
-        "@pages": path.resolve(__dirname, "./src/pages"),
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src"),
+            "@js": resolve(__dirname, "./src/assets/js"),
+            "@css": resolve(__dirname, "./src/assets/css"),
+            "@images": resolve(__dirname, "./src/assets/images"),
+            "@icons": resolve(__dirname, "./src/assets/icons"),
+            "@components": resolve(__dirname, "./src/components"),
+            "@pages": resolve(__dirname, "./src/pages"),
+        },
     },
     plugins: [
         vue(),
